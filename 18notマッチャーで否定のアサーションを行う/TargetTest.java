07fe9214ca37c037860e1cf90cccc8c2f7a43082
saka <49299534+saka-ae@users.noreply.github.com>
2019-05-21 23:46:23 +0900

    add file 18

---
+++ 18notマッチャーで否定のアサーションを行う/TargetTest.java
@@ -0,0 +1,15 @@
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.*;
+
+import org.junit.Test;
+
+public class TargetTest {
+
+	@Test
+	public void notマッチャーで否定のアサーションを行う() {
+		final int num = 1;
+
+		assertThat(num, not(equalTo(num)));
+	}
+
+}
