74dcbdd27062f996a237d8e1466f46d84fbf4f9c
saka <49299534+saka-ae@users.noreply.github.com>
2019-05-22 00:16:40 +0900

    fix file 19

--- 19浮動小数点数の計算は誤差が出る/TargetTest.java
+++ 19浮動小数点数の計算は誤差が出る/TargetTest.java
@@ -1,4 +1,4 @@
-import static org.hamcrest.CoreMatchers.*;
+﻿import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.MatcherAssert.*;
 
 import org.junit.Test;
@@ -6,7 +6,7 @@ import org.junit.Test;
 public class TargetTest {
 
 	@Test
-	public void 浮動小数点の計算は誤差が出る() {
+	public void 浮動小数点数の計算は誤差が出る() {
 		final double wrong    = 0.1 * 3;
 		final double expected = 0.3;
 
