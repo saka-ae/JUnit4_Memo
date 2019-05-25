8fe255089e45fa11934d3cc544c55700f6cb61c8
saka <49299534+saka-ae@users.noreply.github.com>
2019-05-25 10:51:29 +0900

    fix file 35

--- 35独自のカスタムマッチャーで検証する/myCustomMatcher/Within.java
+++ 35独自のカスタムマッチャーで検証する/myCustomMatcher/Within.java
@@ -11,18 +11,21 @@ public class Within extends TypeSafeMatcher<Integer> {
 		_max = max;
 	}
 
+	// assertThat()の第二引数として呼ばれる
 	@Factory
 	public static Matcher<Integer> within(int max) {
 		return new Within(max);
 	}
 
-	public void describeTo(Description description) {
-		description.appendText(_max + "以内");
-	}
-
+	// assertThat()の第一引数を受け取って検証する
 	@Override
 	protected boolean matchesSafely(Integer num) {
 		return num <= _max;
+	}
+
+	// 失敗時に表示されるテキストを定義
+	public void describeTo(Description description) {
+		description.appendText(_max + "以内");
 	}
 
 }
